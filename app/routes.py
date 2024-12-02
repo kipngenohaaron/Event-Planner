447e3805ede3d8c40a1a90774136dd22c5b1ee48
kipngenohaaron <kipngenohaaron@gmail.com>
2024-12-02 14:13:03 +0300

    routes.py added

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,24 @@
+# # from flask import Blueprint, render_template, request, redirect, url_for, flash
+# # from .models import Event
+# # from . import db
+
+# # bp = Blueprint('routes', __name__)
+
+# # @bp.route('/create', methods=['GET', 'POST'])
+# # def create_event():
+# #     if request.method == 'POST':
+# #         title = request.form.get('title')
+# #         description = request.form.get('description')
+# #         date = request.form.get('date')
+# #         location = request.form.get('location')
+
+# #         new_event = Event(title=title, description=description, date=date, location=location)
+# #         db.session.add(new_event)
+# #         db.session.commit()
+# #         flash('Event created successfully!', 'success')
+# #         return redirect(url_for('routes.create_event'))
+
+# #     return render_template('create_event.html')
 # from flask import Blueprint, render_template, request, redirect, url_for, flash
 # from .models import Event
 # from . import db
@@ -19,24 +40,28 @@
 #         return redirect(url_for('routes.create_event'))
 
 #     return render_template('create_event.html')
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for
+from . import app, db
 from .models import Event
-from . import db
 
-bp = Blueprint('routes', __name__)
+@app.route('/')
+def index():
+    events = Event.query.all()
+    return render_template('index.html', events=events)
 
-@bp.route('/create', methods=['GET', 'POST'])
+@app.route('/create', methods=['GET', 'POST'])
 def create_event():
     if request.method == 'POST':
-        title = request.form.get('title')
-        description = request.form.get('description')
-        date = request.form.get('date')
-        location = request.form.get('location')
+        title = request.form['title']
+        description = request.form['description']
+        date = request.form['date']
+        location = request.form['location']
 
+        # Create new event and add to database
         new_event = Event(title=title, description=description, date=date, location=location)
         db.session.add(new_event)
         db.session.commit()
-        flash('Event created successfully!', 'success')
-        return redirect(url_for('routes.create_event'))
+
+        return redirect(url_for('index'))
 
     return render_template('create_event.html')
