b891104e23471ab531b969c8a8e16c14ee832945
kipngenohaaron <kipngenohaaron@gmail.com>
2024-12-02 12:36:07 +0300

    routes.py added

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import render_template, request, redirect, url_for, flash
+from . import db
+from .models import Event
+
+@app.route('/create', methods=['GET', 'POST'])
+def create_event():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        date = request.form.get('date')
+        location = request.form.get('location')
+
+        new_event = Event(title=title, description=description, date=date, location=location)
+        db.session.add(new_event)
+        db.session.commit()
+        flash('Event created successfully!', 'success')
+        return redirect(url_for('create_event'))
+
+    return render_template('create_event.html')
